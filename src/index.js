c37869b96fd771eefc3a0b5ea72c7aa194288d98
jerry <brownjer3@gmail.com>
2021-02-04 09:33:20 -0800

    commit tree 3fe536283efe6b94727fc6b0b99fc18084a8c729

--- src/index.js
+++ src/index.js
@@ -13,3 +13,24 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch("http://localhost:3000/toys")
+  .then(r => r.json())
+  .then(json => renderToys(json))
+})
+
+function renderToys(toys) {
+  let collectionDiv = document.getElementById('toy-collection')
+  for (const toy of toys) {
+    let toyDiv = document.createElement('div')
+    toyDiv.class = "card"
+    toyDiv.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}">
+      <p>${toy.likes}</p>
+      <button class="like-btn">Like <3</button>
+    `
+    collectionDiv.appendChild(toyDiv)
+  }
+}
